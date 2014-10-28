7cf20ec5ba3659c0b674455a07caa87ce4ae53f0
Sherwin Yu <sherwinyu@Sherwins-MacBook-Pro.local>
2014-10-28 00:01:13 -0400

    Initial Commit

---
+++ AcroFinder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AcroFinder
+//
+//  Created by Sherwin Yu on 10/28/14.
+//  Copyright (c) 2014 SherwinYu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
